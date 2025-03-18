52670f6151c88f1f0bd5482f2910edb589f796f7
KodcuAhmet <ahmet.cigil@tuta.com>
2025-03-18 21:28:29 +0100

    angular forms created

--- src/app/site-header/site-header.component.ts
+++ src/app/site-header/site-header.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IUser } from '_course-resources/user/user.model';
+import { UserService } from '_course-resources/user/user.service';
+import { subscribeOn } from 'rxjs';
 
 @Component({
   selector: 'bot-site-header',
   templateUrl: './site-header.component.html',
   styleUrls: ['./site-header.component.css']
 })
-export class SiteHeaderComponent {
+export class SiteHeaderComponent implements OnInit {
+  signOut() {
+    throw new Error('Method not implemented.');
+  }
+  user: IUser | null = null;
+  showSignOutMenu: boolean = false;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
+  ngOnInit() {
+    this.userService.getUser().subscribe({
+      next: (user) => (this.user = user)
+    })
+  }
+
+  toggleSignOutMenu() {
+    this.showSignOutMenu = !this.showSignOutMenu;
+  }
 }
