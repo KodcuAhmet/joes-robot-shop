0f4858362914cf4e38fb0e8612583f01193c70ef
KodcuAhmet <ahmet.cigil@tuta.com>
2025-03-18 15:01:58 +0100

    routing and navigation done

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IProduct } from './product.model';
 import { CartService } from '../cart/cart.service';
 import { ProductService } from './product.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'bot-catalog',
@@ -12,18 +13,25 @@ export class CatalogComponent {
   products: any;
   filter: string = "";
 
-  constructor(private cartSvc: CartService, private productSvc: ProductService) {
-
-  }
+  constructor(
+    private cartSvc: CartService,
+    private productSvc: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
     this.productSvc.getProducts().subscribe(products => {
       this.products = products;
+    });
+    this.route.queryParams.subscribe((params) => {
+      this.filter = params['filter'] ?? '';
     })
   }
 
   addToCart(product: IProduct) {
     this.cartSvc.add(product);
+    this.router.navigate(['/cart']);
   }
 
   getDiscountedClasses(product: IProduct) {
